90f992c4ccb7a62b0fda8b03aaf3fbe1902caea8
vinayakhowale <125206453+vinayakhowale@users.noreply.github.com>
2025-06-25 16:11:01 +0530

    margosaTree

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// sticky navbar
+$(function(){
+    var navbar = $('.header-inner');
+    $(window).scroll(function(){
+      if($(window).scrollTop() <=40){
+        navbar.removeClass('navbar-scroll');
+      }else{
+        navbar.addClass('navbar-scroll');
+      }
+    });
+  });
